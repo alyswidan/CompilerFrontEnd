491850ae6e090761c083e1b1386741cb0b18a6db
alyswidan <swidan12340@gmail.com>
2018-03-16 14:28:38 +0200

    created a special state for nfa

---
+++ src/LexicalAnalyser/NFA/NFAState.java
@@ -0,0 +1,17 @@
+package LexicalAnalyser.NFA;
+
+import LexicalAnalyser.BaseModels.State;
+
+import java.util.List;
+
+/**
+ * Created by alyswidan on 16/03/18.
+ */
+public class NFAState extends State {
+
+    List<State> getEpsilonClosure(State state) {
+        return null;
+    }
+
+
+}
