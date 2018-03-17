a2874d36621820c78974da3b0eaa43c37d2337fe
Admin <amrmohamed53@gmail.com>
2018-03-18 01:57:03 +0200

    updating kleeneClousre class

--- src/LexicalAnalyser/Regex/KleeneClosureOperator.java
+++ src/LexicalAnalyser/Regex/KleeneClosureOperator.java
@@ -20,18 +20,20 @@ public class KleeneClosureOperator implements UnaryRegexOperator {
          * end  = newEnd
          * return updated operand
          */
-        // I did the next 2 lines as the coming NFA may have many start and accept status
-        NFAState start = NFAState.epsilonSource(operand.getStartStates());
-        NFAState end = NFAState.epsilonSink(operand.getAcceptingStates());
+
+        NFAState start = (NFAState) operand.getStartState();
+        NFAState end  = operand.mergeAcceptStates();
 
         NFAState newStart = new NFAState();
         NFAState newEnd = new NFAState();
+
         newStart.addTransition(new EpsilonRegularDefinition(), start);
         end.addTransition(new EpsilonRegularDefinition(), newEnd);
         end.addTransition(new EpsilonRegularDefinition(), start);
         newStart.addTransition(new EpsilonRegularDefinition(), newEnd);
-        start = newStart;
-        end = newEnd;
+
+        operand.setStartState(newStart);
+        operand.setEndState(newEnd);
 
         return operand;
     }
