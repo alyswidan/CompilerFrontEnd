1f4d8c9ab5f66725029614ab481a5ccaff96771f
Admin <amrmohamed53@gmail.com>
2018-03-18 02:01:45 +0200

    RegexOperator changed to scan char by char

--- src/LexicalAnalyser/Regex/RegexIterator.java
+++ src/LexicalAnalyser/Regex/RegexIterator.java
@@ -30,30 +30,55 @@ public class RegexIterator implements Iterator<RegexElement> {
 
         Predicate<Character> isOperator = RegexOperatorFactory::isOperator;
         Predicate<StringBuilder> isRegDef = s->RegularDefinitionsTable.containsKey(s.toString());
+        Predicate<StringBuilder> isEpsilon = s-> s.toString().equals("\\L");
+        Predicate<Character> isEscape = c -> c == '\\';
+        Predicate<Character> isSpace = c-> c == ' ';
+
         char currentChar = regex.rawRegex.charAt(currentIndex);
         boolean entered = false;
         RegexElement regexElement = null;
         StringBuilder regDefCandidate = new StringBuilder();
 
-        while(isOperator.negate().test(currentChar) &&  isRegDef.negate().test(regDefCandidate)) {
-            entered = true;
-            regDefCandidate.append(currentChar);
-
+        while(isSpace.test(currentChar) && hasNext()){
             currentChar = regex.rawRegex.charAt(++currentIndex);
         }
 
+        while(isOperator.negate().test(currentChar)
+                && isRegDef.negate().test(regDefCandidate)
+                && isEscape.negate().test(currentChar)) {
+            entered = true;
+            regDefCandidate.append(currentChar);
+            currentChar = regex.rawRegex.charAt(++currentIndex);
+        }
 
-        if (!entered){
+        if(isEscape.test(currentChar)){
+            currentChar = regex.rawRegex.charAt(++currentIndex);
+            if(currentChar == 'L') {
+                regexElement = new EpsilonRegularDefinition();
+
+            }
+            else {
+                regexElement = new RegularDefinition(""+currentChar);
+            }
+            currentIndex++;
+        }
+        else if (!entered){
             regexElement = RegexOperatorFactory.getOperator(currentChar);
             currentIndex++;
         }
         else{
+
             if (isRegDef.test(regDefCandidate)){
                 regexElement = RegularDefinitionsTable.get(regDefCandidate.toString());
             }
             else{
-                System.out.println("exception");
-                // TODO: 15/03/18 this should throw a parse exception to be created
+                try {
+                    regexElement = new RegularDefinition(regDefCandidate.toString());
+                    RegularDefinitionsTable.put(regDefCandidate.toString(),(RegularDefinition) regexElement);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
         }
 
