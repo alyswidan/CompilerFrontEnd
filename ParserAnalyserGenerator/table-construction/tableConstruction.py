18d27fd971de3bd6d9d023db5c5a1f526fc37151
Amr Mohamed <amrmohamed53@gmail.com>
2018-05-05 19:38:04 +0200

    Update tableConstruction.py
    Table almost done, tested with the lecture example and it seems working

--- ParserAnalyserGenerator/table-construction/tableConstruction.py
+++ ParserAnalyserGenerator/table-construction/tableConstruction.py
@@ -1,13 +1,19 @@
-first = { 'E':{'AT': ['a'],
-               'id': ['x']},
-          'S':{'B':['b'],
-               '(E)':['a', 'x']}
-}
+first = { 'S':{
+                'AbS': ['a', 'c'],
+                'e': ['e'],
+                '\L':['\L']
+               },
+          'A':{
+              'a':['a'],
+              'cAd':['c']
+              }
+        }
 
-follow = {'E':['a'],
-          'S':['x']}
+follow = {'S':['$'],
+          'A':['b', 'd']
+         }
 
-terminals = ['a', 'b', 'x']
+terminals = ['a', 'b', 'c', 'd', 'e']
 
 def constructParsingTable(first, follow, terminals):
     
@@ -22,14 +28,17 @@ def constructParsingTable(first, follow, terminals):
             # check if in first of the non terminal
             if terminal_key in [item for sublist in first[non_terminal_key].values() for item in sublist]:
                 for val in first[non_terminal_key].values():
-                    if terminal_key in val:
+                    if terminal_key in val and terminal_key:
                         entry_in_table[terminal_key] = non_terminal_key + '->' + \
                                                 ''.join([k for k,v in first[non_terminal_key].items() if terminal_key in v]) 
             
             # check if in the follow of the non terminal
             elif terminal_key in follow[non_terminal_key]:
-                entry_in_table[terminal_key] = 'synh'
-            
+                if '\L' not in [item for sublist in first[non_terminal_key].values() for item in sublist]:
+                    entry_in_table[terminal_key] = 'sync'
+                else:
+                    entry_in_table[terminal_key] = non_terminal_key + "->\L"
+                
             # if not in the first nor the follow of the terminal the error
             else:
                 entry_in_table[terminal_key] = 'empty'
