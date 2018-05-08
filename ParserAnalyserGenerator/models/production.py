6dd06d837d523c0d8c58c6b0cc9df8a31175e8dc
alyswidan <swidan12340@gmail.com>
2018-05-08 10:27:52 +0200

    finished the production class

---
+++ ParserAnalyserGenerator/models/production.py
@@ -0,0 +1,52 @@
+class Production:
+    def __init__(self, name, members):
+        """
+            :param name: type=string, name of the production 
+            :param members: type=list(list(Production for non terminals and string for terminals)) 
+            this is the list of Productions 
+            this production resolves to, if the production is
+            E-> AT|+T|\L
+            the expected list is [[prod_A, prod_T], ['+', prod_T],['\L]]
+            where terminals are presented as strings and prod_X indicates
+            a production object with the name X. 
+        """
+
+        self.name = name
+        self.members_dict = {}
+        for member in members:
+            self.add_member(member)
+
+    def get_member(self, name):
+        try:
+            member = self.members_dict[name]
+            return member
+        except KeyError:
+            raise ValueError(f'production {self.name} has no member {name}')
+
+        
+
+    def remove_member(self, name):
+        try:
+            del self.members_dict[name]
+        except KeyError:
+            raise ValueError(f'production {self.name} has no member {name}') 
+    
+    def add_member(self, member):
+        """
+            add a member list to the production
+            :param member: type=list(Production for non terminals and string for terminals)
+        """
+
+        member_name = ''
+        for part in member:
+            if isinstance(part, str):
+                member_name += part
+            elif isinstance(part, Production):
+                member_name += part.name
+            else:
+                raise ValueError(f'object of type {type(part)} is not a prodution nor a string')
+
+        self.members_dict[member_name] = member
+
+    def __str__(self):
+        return ' | '.join(self.members_dict.keys())
